bd7fec0985d87778ab8d3393de8aa637ce671aef
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2025-05-06 13:24:42 +0200

    now works since the old widget_test was deleted and flutter clean executed

--- integration_test/widget_test.dart
+++ integration_test/widget_test.dart
@@ -1,13 +1,5 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:counter_app/main.dart';
 import 'package:integration_test/integration_test.dart';
 
@@ -22,7 +14,6 @@ void main() {
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
 
     // Finds the floating action button to tap on.
     final fab = find.byKey(const ValueKey('increment'));
@@ -34,7 +25,6 @@ void main() {
     await tester.pumpAndSettle();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
 }
